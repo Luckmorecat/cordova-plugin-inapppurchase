5ad5eaeed10d6973b7dee2f65f3a990dd39923d5
Mikhail Veretnitskii <mikhail.v@upland.me>
2020-08-03 18:46:54 +0300

    Updating code base

--- src/ios/UplandAnalytic.m
+++ src/ios/UplandAnalytic.m
@@ -33,15 +33,20 @@
 }
 
 - (void)didDeferTransaction:(SKPaymentTransaction *)transaction {
+    
+}
 
+- (NSString *)getReceipt {
+    NSURL *receiptUrl = [RMStore receiptURL];
+    NSData *receiptData = [NSData dataWithContentsOfURL:receiptUrl];
+    
+    return [receiptData base64EncodedStringWithOptions:0];
 }
 
 -(void)verifyTransaction:(SKPaymentTransaction *)transaction success:(void (^)())successBlock failure:(void (^)(NSError *))failureBlock {    
-    [_store refreshReceipt];
-    NSURL *receiptUrl = [RMStore receiptURL];
-    NSData *receiptData = [NSData dataWithContentsOfURL:receiptUrl];
+    NSString *receipt = [self getReceipt];
  
-    [_webView evaluateJavaScript:[NSString stringWithFormat:@"window._verifyIosTransaction('%@', '%@')", transaction.transactionIdentifier, [receiptData base64EncodedStringWithOptions:0]] completionHandler:^(id someId, NSError *error) { 
+    [_webView evaluateJavaScript:[NSString stringWithFormat:@"window._verifyIosTransaction('%@', '%@')", transaction.transactionIdentifier, receipt] completionHandler:^(id someId, NSError *error) {
         if (error != nil) {
             NSLog(@"EVALUTE ERROR: %ld", (long)error.code);
         }
